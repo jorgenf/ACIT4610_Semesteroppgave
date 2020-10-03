6a61fb892cadf2992dee15ba897bf743ed3d52c5
Håkon Weydahl <hweydahl@gmail.com>
2020-10-03 10:30:54 +0200

    towards plotting

--- fitness/spike_count.py
+++ fitness/spike_count.py
@@ -1,32 +1,44 @@
-from operator import itemgetter
+def read_recording():
+    from operator import itemgetter
+    import numpy as np
+    # example file
+    filename = r"Data\Dense - 2-1-20.spk.txt"
+    f = open(filename, "r")
+    data_points = [line.split(" ") for line in f]
+    
+    # sort by electrode
+    data_points.sort(key=itemgetter(1))
 
-# example file
-# filename = r"D:\OneDrive\Dokumenter\skole\ASIC\ACIT4410 Evo AI\semesteroppgave\git\Data\Dense - 2-1-20.spk.txt"
-filename = r"C:\Users\weyhak\OneDrive - Bane NOR\Dokumenter\GitHub\ACIT4610_Semesteroppgave\Data\Dense - 2-1-20.spk.txt"
-f = open(filename, "r")
-data_points = [line.split(" ") for line in f]
- 
-# sort by electrode
-data_points.sort(key=itemgetter(1))
+    # cleaning data
+    data_points = [(float(row[0].rstrip()), int(row[1].rstrip())) for row in data_points]
 
-# cleaning data
-data_points = [(float(row[0].rstrip()), int(row[1].rstrip())) for row in data_points]
+    # sort data by electrode ID
+    data_by_electrode = {i:[] for i in range(59+1)} # 59 electrodes in experiment, but both 0 and 59 is present?
+    for row in data_points:
+        data_by_electrode[row[1]].append(row[0])
+    # create numpy array for matplotlib
+    spike_array = np.array([np.array(x) for x in [*data_by_electrode.values()]])
+    
 
-# sort data by electrode ID
-data_by_electrode = {i:[] for i in range(59+1)} # 59 electrodes in experiment, but both 0 and 59 is present?
-for row in data_points:
-    data_by_electrode[row[1]].append(row[0])
+    # count average fire rate per electrode (spikes per second)
+    spike_rates = {key:[] for key in data_by_electrode}
+    for key in data_by_electrode:
+        f_c = len(data_by_electrode[key]) / (30*60) # 30 min recording
+        spike_rates[key] = f_c # spike rate
+    spike_rates_array = np.array([*spike_rates.values()])
 
-# count average fire rate per electrode (spikes per second)
-spike_rates = {key:[] for key in data_by_electrode}
-for key in data_by_electrode:
-    f_c = len(data_by_electrode[key]) / (30*60) # 30 min recording
-    spike_rates[key] = f_c # spike rate
 
-for key in spike_rates:
-    print(key, spike_rates[key])
+    # for key in spike_rates:
+    #     print(key, spike_rates[key])
+    # spike_rate_all_electrodes = sum(spike_rates.values()) / len(spike_rates.values())
+    # print("All: ", spike_rate_all_electrodes)
 
-spike_rate_all_electrodes = sum(spike_rates.values()) / len(spike_rates.values())
-print("All: ", spike_rate_all_electrodes)
+    f.close
+    return (spike_array, spike_rates_array)
 
-f.close
+
+    
+if __name__ == "__main__":
+    spikes, spike_rates = read_recording()
+    print(spikes[:3][:5])
+    print(spike_rates[:3])
