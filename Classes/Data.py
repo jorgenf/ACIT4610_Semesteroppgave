8cb9594cfeea8d90fd494928ee2866aa30a4519a
hawey <hweydahl@gmail.com>
2020-10-25 13:57:08 +0100

    add function raster_plot()

--- Classes/Data.py
+++ Classes/Data.py
@@ -27,4 +27,25 @@ def get_spikes_file(file_name):
     return(firing_rate)
 
 def get_spikes_pheno(phenotype):
-    pass
+    """
+    
+    :param string_one: 
+    :return: NumpyArray
+    """""
+
+    # times = []
+    # nodes = []
+    spikes = []
+
+    for timestamp in phenotype["t"]:
+        # time, node = line.split(" ")
+        # times.append(row[0])
+        # nodes.append(row[1])
+        spikes.append(int(float(timestamp)*10)) # 1/10 second bins
+    firing_rate = []
+    for n in spikes:
+        if n >= len(firing_rate):
+            firing_rate.append(1)
+        else:
+            firing_rate[n] += 1
+    return (firing_rate)
