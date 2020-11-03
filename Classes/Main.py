679472277540d70d58827e584e58ab0d1d6dea4a
hawey <hweydahl@gmail.com>
2020-11-03 22:55:09 +0100

    cosmetics

--- Classes/Main.py
+++ Classes/Main.py
@@ -1,10 +1,19 @@
-import CellularAutomataModel, Population, Data, Fitness, Evolution, Summary
-from multiprocessing import Pool, current_process
 import os
+import time
+from multiprocessing import Pool, current_process
+
 import numpy as np
 import matplotlib.pyplot as plt
-import time
 
+import CellularAutomataModel, Population, Data, Fitness, Evolution, Summary
+
+
+"""
+PARAMETERS
+
+"""
+
+# TYPE parameter sets properties specific for each model type
 TYPE = {
     # Set properties for CA model
     "CA": (
@@ -34,54 +43,62 @@ TYPE = {
             ))
 }
 
-# Set general parameters
+# Set general parameters for the evolutionary algorithm
 evolution_parameters = {
     # "MODEL_TYPE": TYPE["CA"],
-     "MODEL_TYPE": TYPE["Network"],
+    "MODEL_TYPE": TYPE["Network"],
     # Size of array
     "DIMENSION": 10,
     #Number of individuals in the population
-    "POPULATION_SIZE" : 30,
+    "POPULATION_SIZE" : 5,
     #Number of generations to run. Each generation will run a number og simulations equal to POPULATION_SIZE
-    "NUM_GENERATIONS": 22,
+    "NUM_GENERATIONS": 1,
     #Simulation duration in seconds
     "SIMULATION_DURATION": 10,
     #Number of simulation iterations per second
-    "TIME_STEP_RESOLUTION": 1,
+    "TIME_STEP_RESOLUTION": 10,
     #Chance for mutation for each gene selection
     "MUTATION_P": 0.1,
     #Percentage of current population that will create offspring
     "PARENTS_P": 0.5,
     #Percentage of current population that will be included in next generation
     "RETAINED_ADULTS_P": 0.1,
-    #Type of fitness function used. NOT IMPLEMENTED!
-    "FITNESS_FUNCTION": "Normalized cross correlation",
+    #Name of the file of experimental data used as reference for fitness function and raster plot
     "REFERENCE_PHENOTYPE": "Small - 7-1-35.spk.txt"
 }
 
-if __name__ == '__main__':
-#Creates threads of run_thread method. Pool-size = threads - 1. Each thread result is mapped to a variable that is
-#returned when all processes are finished
+
+
+"""
+PROGRAM
+
+"""
+if __name__ == "__main__":
     start_time = time.time()
+
+    #Creates threads of run_thread method. Pool-size = threads - 1. Each thread result is mapped to a variable that is
+    #returned when all processes are finished
     def run_threads(individuals):
         p = Pool(os.cpu_count()-1)
         new_individuals = p.map(evo.generate_phenotype, individuals)
         return new_individuals
+
+    #Creates population object with POPULATION_SIZE. 
+    #Creates the set of genes that apply to this specific population
+    pop = Population.Population(
+        evolution_parameters["POPULATION_SIZE"], 
+        evolution_parameters["MODEL_TYPE"][1]
+        )
+
+    # Creates Evolution object with parameters set 
+    evo = Evolution.Evolution(evolution_parameters)
 
     # Inititate variables
     fitness_trend = []
     average_fitness_trend = []
     parameter_trend = []
 
-    #Creates population object with POPULATION_SIZE. Runs loop for NUM_GENERATIONS.
-    #Creates the set of genes that apply to this specific population
-    pop = Population.Population(
-        evolution_parameters["POPULATION_SIZE"], 
-        evolution_parameters["MODEL_TYPE"][1]
-        )
-    evo = Evolution.Evolution(evolution_parameters)
-
-    # Start the evolution
+    # Start the evolution. Runs loop for NUM_GENERATIONS
     print("Running simulation...")
     for i in range(evolution_parameters["NUM_GENERATIONS"]):
         print("\nGeneration:", i)
@@ -102,10 +119,10 @@ if __name__ == '__main__':
         else:
             pop.individuals = pop_with_phenotypes
 
-    pop.individuals.sort(key=lambda x: x.fitness, reverse=True)
-    best = pop.individuals[0]
+    # Register the time it took to run the script
     end_time = time.time()
     total_time = end_time - start_time
+
     # Save summary
     summary = Summary.Summary(pop, evolution_parameters)
     summary.raster_plot()
@@ -113,3 +130,5 @@ if __name__ == '__main__':
     summary.parameter_trend_plot(parameter_trend)
     summary.average_distance_plot()
     summary.output_text(total_time)
+
+
