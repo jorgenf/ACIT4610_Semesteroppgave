a30dae1c89258145c370c4eca43bf01d9458991d
Ruben Jahren <jegsomhetermeg@gmail.com>
2021-10-11 13:05:30 +0200

    Update raster_ASDR.py

--- Code/plots/raster_ASDR.py
+++ Code/plots/raster_ASDR.py
@@ -14,10 +14,16 @@ import Code.Data as Data
 
 sim_length = 1800
 sim_start = 0
+#  Model Type
+model_type = "CA"
 # where to find data
+culture_name = "ca_small_6-2"
+evo_folder_name = "ca_dim10_pop60_gen80_dur60_res40_mut0.1_par0.5_ret0.05_6-2-10_20210808074215"
+#  Path magic
 pickle_name = Path("plots/pickles/20210818")
 experiment_folder = Path("E:/Library/Documents/OneDrive - OsloMet/SSCI ICES paper/NEW NEW RESULTS - Now with more results")
-sub_folder = Path(experiment_folder, "ca_dense_2-1/BEST/ca_dim10_pop60_gen120_sta40_dur60_res40_mut0.1_par0.5_ret0.05_2-1-31_20210818221715/single_runs")
+culture_folder = Path(experiment_folder, culture_name, "BEST")
+single_runs_folder = Path(culture_folder, evo_folder_name, "single_runs")
 # where to save plots
 savepath = Path("plots/figures/raster_ASDR")
 
@@ -27,18 +33,21 @@ width = 7.16
 height = 2.5
 aspect_ratio = 10
 font_size = 10
+dpi = 500
+raster_width = 0.5
 
 # set color theme
 model_palette = {
+	#  "CA": "#509A29",  # green
 	"CA": "#509A29",  # green
 	"Network": "#2A74BC"  # blue
 }
 
 savepath.mkdir(parents=True, exist_ok=True)
 #  df = pd.read_pickle(Path(f"{pickle_name}/individual_data.pkl"))
-df = pd.read_pickle(Path(f"{pickle_name}/pheno_data.pkl"))
+#  df = pd.read_pickle(Path(f"{pickle_name}/pheno_data.pkl"))
 
-spikes_file = Data.get_spikes_file(Path(sub_folder, "0.txt"), recording_start=sim_start, recording_len=sim_length, fullpath=True)
+spikes_file = Data.get_spikes_file(Path(single_runs_folder, "0.txt"), recording_start=sim_start, recording_len=sim_length, fullpath=True)
 spike_rate = Data.get_spikerate(spikes_file, sim_length, sim_start)
 
 #   Check if input is in the correct format
@@ -57,7 +66,7 @@ fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex="all", sharey="row", fig
 #   Make raster plots
 ax1.eventplot(
    A_spikes_per_array,
-   linewidths=0.5, color="black"
+   linewidths=raster_width, color=model_palette[model_type]
 )
 #  ax1.set_xlabel("Minutes", fontsize=font_size)
 #  ax1.set_ylabel("Electrode", fontsize=font_size)
@@ -69,7 +78,7 @@ line_y = spike_rate
 ax2.plot(
 	line_x,
 	line_y,
-	color="black", lw=0.2
+	color=model_palette[model_type], lw=0.2
 )
 ax2.set_xlabel("Minutes", fontsize=font_size)
 #  ax2.set_ylabel("Spikes / M", fontsize=font_size)
@@ -81,9 +90,10 @@ ax2.set_position([1, 1, 1, 1])
 #ax2.set_xlabel("Seconds")
 #ax2.set_ylabel("Spikes per second")
 
-ax1_y_limit = ax1.get_ylim()
-ax2_y_limit = ax2.get_ylim()
-ax2_y_limit = (math.ceil(ax2_y_limit[0]), math.floor(ax2_y_limit[1]))
+#  ax1_y_limit = ax1.get_ylim()
+#  ax2_y_limit = ax2.get_ylim()
+#  ax2_y_limit = (math.ceil(ax2_y_limit[0]), math.floor(ax2_y_limit[1]))
+ax2_y_limit = (spike_rate.min(), spike_rate.max())
 
 formatter = ticker.FuncFormatter(lambda s, x: time.strftime('%M', time.gmtime(s)))
 ax1.xaxis.set_major_locator(plt.FixedLocator((sim_start, sim_length), sim_length))
@@ -96,10 +106,15 @@ ax2.yaxis.set_major_locator(plt.FixedLocator((ax2_y_limit[0], ax2_y_limit[1]), a
 #fig.set_figheight(6)
 #fig.set_figwidth(7.16)
 fig.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=0.5)
-fig.savefig(Path(savepath, "0"))
+fig.savefig(Path(savepath, culture_name), dpi=dpi)
 plt.close()
 
-
+np.set_printoptions(threshold=np.inf)
+#  print("SPIKE FILE\n{}\n".format(len(spikes_file)))
+#  print("SPIKE RATE\n{}\n".format(spike_rate.max()))
+#  print("SPIKES PER ARRAY\n{}\n".format(len(A_spikes_per_array)))
+#  print(ax1_y_limit)
+#  print(ax2_y_limit)
 
 '''
 total_n_plots = len(df["Culture"].unique()) * len(df["DIV"].unique()) * 2
